74a422c49079886d45b3b31a5d42f4c0679217c5
Karl-Martin Skontorp <kms@skontorp.net>
2009-02-15 02:08:23 +0100

    Retab everything.

--- functions.php
+++ functions.php
@@ -20,9 +20,9 @@ function printHeader() {
     print("<head>\n");
     print("<title>SimpleRSS</title>\n");
     print("<link rel=\"stylesheet\" href=\"simplerss.css\""
-	    . "type=\"text/css\" />\n");
+            . "type=\"text/css\" />\n");
     print("<meta http-equiv=\"Content-Type\" "
-	    . "content=\"text/html; charset=UTF-8\" />\n");
+            . "content=\"text/html; charset=UTF-8\" />\n");
     print("</head>\n\n");
 
     print("<body>\n\n");
@@ -48,13 +48,13 @@ function printHeader() {
     print("\">Refresh all</a>\n");
     print("|\n");
     if ($_GET["list"] == "1") {
-	print("<a href=\"");
-	print(htmlentities(getNewUrl($column)));
-	print("\">Normal view</a>\n");
+        print("<a href=\"");
+        print(htmlentities(getNewUrl($column)));
+        print("\">Normal view</a>\n");
     } else {
-	print("<a href=\"");
-	print(htmlentities(getNewUrl($column)) . "&list=1");
-	print("\">List feeds</a>\n");
+        print("<a href=\"");
+        print(htmlentities(getNewUrl($column)) . "&list=1");
+        print("\">List feeds</a>\n");
     }
     print("|\n");
     print("<a href=\"" . getBaseUrl() . "\">Clear</a>\n");
@@ -68,25 +68,25 @@ function printHeader() {
     print("<input type=\"submit\" value=\"Add feed\" />\n");
 
     if (sizeof($column) > 0) {
-	$i = 0;
+        $i = 0;
 
-	foreach ($column as $col) {
-	    if (is_array($col)) {
-		print("<input type=\"hidden\" name=\"column$i\" ");
-		print("value=\"");
-		foreach ($col as $url) {
-		    if ($url["url"] != "") {
-			print(htmlentities($url["url"]));
-			print(" " . $url["maxItems"]);
-			print("\n");
-		    }
-		}
-		print("\"/>\n");
-		$i++;
-	    }
-	}
+        foreach ($column as $col) {
+            if (is_array($col)) {
+                print("<input type=\"hidden\" name=\"column$i\" ");
+                print("value=\"");
+                foreach ($col as $url) {
+                    if ($url["url"] != "") {
+                        print(htmlentities($url["url"]));
+                        print(" " . $url["maxItems"]);
+                        print("\n");
+                    }
+                }
+                print("\"/>\n");
+                $i++;
+            }
+        }
 
-	print("</form>\n\n");
+        print("</form>\n\n");
     }
     print("</div>\n\n");
     print("<div class=\"tmp\" />");
@@ -109,7 +109,7 @@ function getBaseUrl() {
     $baseUrl .= $_SERVER["SERVER_NAME"];
 
     if ($_SERVER["SERVER_PORT"] != "80") {
-	$baseUrl .= ":" . $_SERVER["SERVER_PORT"];
+        $baseUrl .= ":" . $_SERVER["SERVER_PORT"];
     }
 
     $baseUrl .= eregi_replace("(.*)index.php", "\\1", $_SERVER["SCRIPT_NAME"]);
@@ -126,22 +126,22 @@ function getNewUrl($column) {
     $newUrl .= "?";
 
     if (is_array($column)) {
-	$i = 0;
+        $i = 0;
 
-	foreach ($column as $col) {
-	    if (is_array($col)) {
-		$newUrl .= "column$i=";
-		foreach ($col as $url) {
-		    if ($url["url"] != "") {
-			$newUrl .= rawurlencode($url["url"]);
-			$newUrl .= rawurlencode(" ") . rawurlencode($url["maxItems"]);
-			$newUrl .= rawurlencode("\n");
-		    }
-		}
-		$newUrl .= "&";
-		$i++;
-	    }
-	}
+        foreach ($column as $col) {
+            if (is_array($col)) {
+                $newUrl .= "column$i=";
+                foreach ($col as $url) {
+                    if ($url["url"] != "") {
+                        $newUrl .= rawurlencode($url["url"]);
+                        $newUrl .= rawurlencode(" ") . rawurlencode($url["maxItems"]);
+                        $newUrl .= rawurlencode("\n");
+                    }
+                }
+                $newUrl .= "&";
+                $i++;
+            }
+        }
     }
 
     return $newUrl;
@@ -151,24 +151,24 @@ function parseColumns($get) {
     $i = 0;
 
     while (isSet($get["column$i"])) {
-	$tok = strtok($get["column$i"], "\n");
+        $tok = strtok($get["column$i"], "\n");
 
-	$urls = null;
-	$j = 0;
-	/* Get all URLs and add them to an array. */
-	while ($tok) {
-	    $urlSplitted = split(" ", $tok);
+        $urls = null;
+        $j = 0;
+        /* Get all URLs and add them to an array. */
+        while ($tok) {
+            $urlSplitted = split(" ", $tok);
 
-	    $column[$i][$j]["url"] = trim($urlSplitted[0]);
-	    if (is_numeric(trim($urlSplitted[1])) && trim($urlSplitted[1] >= 0)) {
-		$column[$i][$j]["maxItems"] = trim($urlSplitted[1]);
-	    } else {
-		$column[$i][$j]["maxItems"] = $DEFAULT_MAXITEMS;
-	    }
-	    $j++;
-	    $tok = strtok("\n");
-	}
-	$i++;
+            $column[$i][$j]["url"] = trim($urlSplitted[0]);
+            if (is_numeric(trim($urlSplitted[1])) && trim($urlSplitted[1] >= 0)) {
+                $column[$i][$j]["maxItems"] = trim($urlSplitted[1]);
+            } else {
+                $column[$i][$j]["maxItems"] = $DEFAULT_MAXITEMS;
+            }
+            $j++;
+            $tok = strtok("\n");
+        }
+        $i++;
     }
     return $column;
 }
@@ -184,42 +184,42 @@ function getFeed($feed, $x, $y) {
     $cacheFile = "$CACHE_DIR/$urlMD5";
 
     if (isset($_GET["refreshx"]) && isset($_GET["refreshy"]) 
-	    && ($_GET["refreshx"] == $x) 
-	    && ($_GET["refreshy"] == $y)) {
-	$cacheAge = 65536;
+            && ($_GET["refreshx"] == $x) 
+            && ($_GET["refreshy"] == $y)) {
+        $cacheAge = 65536;
     } else if ($_GET["refreshall"] == "1") {
-	$cacheAge = 65536;
+        $cacheAge = 65536;
     } else {
-	if (file_exists($cacheFile)) {
-	    $cacheFileStat = stat($cacheFile);
-	    $cacheAge = (time() - $cacheFileStat["ctime"]);
-	} else {
-	    $cacheAge = 65536;
-	}
+        if (file_exists($cacheFile)) {
+            $cacheFileStat = stat($cacheFile);
+            $cacheAge = (time() - $cacheFileStat["ctime"]);
+        } else {
+            $cacheAge = 65536;
+        }
     }
 
     if ($cacheAge < $DEFAULT_MAX_AGE) {
-	$handle = fopen($cacheFile, "rb");
-	$data["data"] = fread($handle, filesize($cacheFile));
-	$data["age"] = $cacheAge;
+        $handle = fopen($cacheFile, "rb");
+        $data["data"] = fread($handle, filesize($cacheFile));
+        $data["age"] = $cacheAge;
     } else {
-	$ch = curl_init();
-	//curl_setopt($ch, CURLOPT_PROXY, 
-	//"http://home.tmvs.vgs.no:3128/");
-	curl_setopt($ch, CURLOPT_URL, $feed["url"]);
-	curl_setopt($ch, CURLOPT_HEADER, 0);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_USERAGENT, "SimpleRSS/$VERSION http://skontorp.net/simplerss/");
-	curl_setopt($ch, CURLOPT_ENCODING, "");
-	$data["data"] = curl_exec($ch);
-	curl_close($ch);
-	$tmpname = tempnam($CACHE_DIR, "simplerss-");
-	$tmpfile = fopen($tmpname, "wb");
-	fwrite($tmpfile, $data["data"]);
-	fclose($tmpfile);
-	rename($tmpname, $cacheFile);
+        $ch = curl_init();
+        //curl_setopt($ch, CURLOPT_PROXY, 
+        //"http://home.tmvs.vgs.no:3128/");
+        curl_setopt($ch, CURLOPT_URL, $feed["url"]);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_USERAGENT, "SimpleRSS/$VERSION http://skontorp.net/simplerss/");
+        curl_setopt($ch, CURLOPT_ENCODING, "");
+        $data["data"] = curl_exec($ch);
+        curl_close($ch);
+        $tmpname = tempnam($CACHE_DIR, "simplerss-");
+        $tmpfile = fopen($tmpname, "wb");
+        fwrite($tmpfile, $data["data"]);
+        fclose($tmpfile);
+        rename($tmpname, $cacheFile);
 
-	$data["age"] = 0;
+        $data["age"] = 0;
     }
 
     return $data;
--- index.php
+++ index.php
@@ -16,8 +16,8 @@ $DEFAULT_MAX_AGE = 1800;
 $CACHE_DIR = "cache";
 
 $ALPHA = array("a", "b", "c", "d", "e", "f", "g", "h", 
-	"i", "j", "k", "l", "m", "n", "o", "p", "q", 
-	"r", "s", "t", "u", "v", "w", "x", "y", "z");
+        "i", "j", "k", "l", "m", "n", "o", "p", "q", 
+        "r", "s", "t", "u", "v", "w", "x", "y", "z");
 
 $VERSION = "0.9";
 
@@ -27,11 +27,11 @@ if ($_GET["list"] == "1") {
     printHeader();
     print("<p>");
     foreach ($column as $col) {
-	if (is_array($col)) {
-	    foreach($col as $feed) {
-		print($feed["url"] . "<br/>");
-	    }
-	}
+        if (is_array($col)) {
+            foreach($col as $feed) {
+                print($feed["url"] . "<br/>");
+            }
+        }
     }
     print("</p>");
     printFooter();
@@ -63,146 +63,146 @@ $COLUMN_WIDTH = round(100 / sizeof($column));
 foreach ($column as $x => $col) {
 
     if (is_array($col)) {
-	print("<div class=\"blog\" style=\"width: $COLUMN_WIDTH%;\">\n");
+        print("<div class=\"blog\" style=\"width: $COLUMN_WIDTH%;\">\n");
 
-	/* Iterate through elements. */
-	foreach($col as $y => $feed) {
+        /* Iterate through elements. */
+        foreach($col as $y => $feed) {
 
-	    $data = getFeed($feed, $x, $y);
+            $data = getFeed($feed, $x, $y);
 
-	    $p = new feedParser();
-	    $info = $p->parseFeed($data["data"]);
+            $p = new feedParser();
+            $info = $p->parseFeed($data["data"]);
 
-	    if (sizeof($info["item"]) > 0) {
-		print("<div class=\"blogTitle\">");
-		if ($info["channel"]["link"] != "") {
-		    print("<a href=\"" . $info["channel"]["link"]
-			    . "\">"
-			    . $info["channel"]["title"] . "</a>\n");
-		} else {
-		    print($info["channel"]["title"] . "\n");
-		}
-		print("<a href=\"" . $feed["url"] . "\">\n");
-		print("<img src=\"xml-tiny.png\" /></a></div>");
-		print("<div class=\"manage\">(");
-		print($x . $ALPHA[$y] . ":&nbsp;\n");
+            if (sizeof($info["item"]) > 0) {
+                print("<div class=\"blogTitle\">");
+                if ($info["channel"]["link"] != "") {
+                    print("<a href=\"" . $info["channel"]["link"]
+                            . "\">"
+                            . $info["channel"]["title"] . "</a>\n");
+                } else {
+                    print($info["channel"]["title"] . "\n");
+                }
+                print("<a href=\"" . $feed["url"] . "\">\n");
+                print("<img src=\"xml-tiny.png\" /></a></div>");
+                print("<div class=\"manage\">(");
+                print($x . $ALPHA[$y] . ":&nbsp;\n");
 
-		print("<a href=\"");
-		$tmp = $column;
-		if (!is_array($tmp[$x-1])) {
-		    $tmp[$x-1] = array();
-		}
-		array_push($tmp[$x-1], $tmp[$x][$y]);
-		unset($tmp[$x][$y]);
-		print(htmlentities(getNewUrl($tmp)));
-		print("\">L</a>&nbsp;\n");
+                print("<a href=\"");
+                $tmp = $column;
+                if (!is_array($tmp[$x-1])) {
+                    $tmp[$x-1] = array();
+                }
+                array_push($tmp[$x-1], $tmp[$x][$y]);
+                unset($tmp[$x][$y]);
+                print(htmlentities(getNewUrl($tmp)));
+                print("\">L</a>&nbsp;\n");
 
-		print("<a href=\"");
-		$tmp = $column;
-		if (!is_array($tmp[$x+1])) {
-		    $tmp[$x+1] = array();
-		}
-		array_push($tmp[$x+1], $tmp[$x][$y]);
-		unset($tmp[$x][$y]);
-		print(htmlentities(getNewUrl($tmp)));
-		print("\">R</a>&nbsp;\n");
+                print("<a href=\"");
+                $tmp = $column;
+                if (!is_array($tmp[$x+1])) {
+                    $tmp[$x+1] = array();
+                }
+                array_push($tmp[$x+1], $tmp[$x][$y]);
+                unset($tmp[$x][$y]);
+                print(htmlentities(getNewUrl($tmp)));
+                print("\">R</a>&nbsp;\n");
 
-		if (isset($tmp[$x][$y-1])) {
-		    print("<a href=\"");
-		    $tmp = $column;
-		    $tmpFeed = $tmp[$x][$y];
-		    $tmp[$x][$y] = $tmp[$x][$y-1];
-		    $tmp[$x][$y-1] = $tmpFeed;
-		    print(htmlentities(getNewUrl($tmp)));
-		    print("\">U</a>&nbsp;\n");
-		} else {
-		    print("U&nbsp;\n");;
-		}
+                if (isset($tmp[$x][$y-1])) {
+                    print("<a href=\"");
+                    $tmp = $column;
+                    $tmpFeed = $tmp[$x][$y];
+                    $tmp[$x][$y] = $tmp[$x][$y-1];
+                    $tmp[$x][$y-1] = $tmpFeed;
+                    print(htmlentities(getNewUrl($tmp)));
+                    print("\">U</a>&nbsp;\n");
+                } else {
+                    print("U&nbsp;\n");;
+                }
 
-		if (isset($tmp[$x][$y+1])) {
-		    print("<a href=\"");
-		    $tmp = $column;
-		    $tmpFeed = $tmp[$x][$y];
-		    $tmp[$x][$y] = $tmp[$x][$y+1];
-		    $tmp[$x][$y+1] = $tmpFeed;
-		    print(htmlentities(getNewUrl($tmp)));
-		    print("\">D</a>)&nbsp;\n");
-		} else {
-		    print("D)&nbsp;\n");;
-		}
+                if (isset($tmp[$x][$y+1])) {
+                    print("<a href=\"");
+                    $tmp = $column;
+                    $tmpFeed = $tmp[$x][$y];
+                    $tmp[$x][$y] = $tmp[$x][$y+1];
+                    $tmp[$x][$y+1] = $tmpFeed;
+                    print(htmlentities(getNewUrl($tmp)));
+                    print("\">D</a>)&nbsp;\n");
+                } else {
+                    print("D)&nbsp;\n");;
+                }
 
-		print("(<a href=\"");
-		$tmp = $column;
-		unset($tmp[$x][$y]);
-		print(htmlentities(getNewUrl($tmp)));
-		print("\">Del</a>)&nbsp;\n");
+                print("(<a href=\"");
+                $tmp = $column;
+                unset($tmp[$x][$y]);
+                print(htmlentities(getNewUrl($tmp)));
+                print("\">Del</a>)&nbsp;\n");
 
-		print("(" . $feed["maxItems"] . ":&nbsp;");
-		print("<a href=\"");
-		$tmp = $column;
-		$tmp[$x][$y]["maxItems"]++;
-		print(htmlentities(getNewUrl($tmp)));
-		print("\">+</a>/");
-		if ($feed["maxItems"] > 0) {
-		    print("<a href=\"");
-		    $tmp = $column;
-		    $tmp[$x][$y]["maxItems"]--;
-		    print(htmlentities(getNewUrl($tmp)));
-		    print("\">-</a>)\n");
-		} else {
-		    print("-)\n");
-		}
+                print("(" . $feed["maxItems"] . ":&nbsp;");
+                print("<a href=\"");
+                $tmp = $column;
+                $tmp[$x][$y]["maxItems"]++;
+                print(htmlentities(getNewUrl($tmp)));
+                print("\">+</a>/");
+                if ($feed["maxItems"] > 0) {
+                    print("<a href=\"");
+                    $tmp = $column;
+                    $tmp[$x][$y]["maxItems"]--;
+                    print(htmlentities(getNewUrl($tmp)));
+                    print("\">-</a>)\n");
+                } else {
+                    print("-)\n");
+                }
 
-		print("&nbsp;(");
-		if ($data["age"] < ($DEFAULT_MAX_AGE * 0.1)) {
-		    print("<span class=\"cacheAge10\">");
-		} else if ($data["age"] < ($DEFAULT_MAX_AGE * 0.9)) {
-		    print("<span class=\"cacheAge90\">");
-		} else {
-		    print("<span class=\"cacheAge100\">");
-		}
+                print("&nbsp;(");
+                if ($data["age"] < ($DEFAULT_MAX_AGE * 0.1)) {
+                    print("<span class=\"cacheAge10\">");
+                } else if ($data["age"] < ($DEFAULT_MAX_AGE * 0.9)) {
+                    print("<span class=\"cacheAge90\">");
+                } else {
+                    print("<span class=\"cacheAge100\">");
+                }
 
-		print($data["age"]);
-		print(" s.:&nbsp;");
-		print("</span>\n");
-		print("<a href=\"");
-		print(htmlentities(getNewUrl($tmp)) 
-			. "&amp;refreshx=$x&amp;refreshy=$y");
-		print("\">Ref</a>)\n");
+                print($data["age"]);
+                print(" s.:&nbsp;");
+                print("</span>\n");
+                print("<a href=\"");
+                print(htmlentities(getNewUrl($tmp)) 
+                        . "&amp;refreshx=$x&amp;refreshy=$y");
+                print("\">Ref</a>)\n");
 
-		print("</div>\n");
+                print("</div>\n");
 
-		print("<ul>\n");
+                print("<ul>\n");
 
-		$k = 0;
+                $k = 0;
 
-		foreach ($info["item"] as $val) {
-		    $k++;
+                foreach ($info["item"] as $val) {
+                    $k++;
 
-		    if ($k > $feed["maxItems"]) {
-			break;
-		    }
+                    if ($k > $feed["maxItems"]) {
+                        break;
+                    }
 
-		    if($val["link"] != "") {
-			print("<li><a href=\"" 
-				. $val["link"] . "\">" 
-				. $val["title"] . "</a>"
-				. "</li>\n");
-		    } else {
-			print("<li>" . $val["title"]
-				. "</li>\n");
-		    }
-		}
+                    if($val["link"] != "") {
+                        print("<li><a href=\"" 
+                                . $val["link"] . "\">" 
+                                . $val["title"] . "</a>"
+                                . "</li>\n");
+                    } else {
+                        print("<li>" . $val["title"]
+                                . "</li>\n");
+                    }
+                }
 
-		print("</ul>\n");
-	    } else {
-		$l = sizeof($errors);
-		$errors[$l]["url"] = $feed["url"];
-		$errors[$l]["x"] = $x;
-		$errors[$l]["y"] = $y;
-	    }
-	}
-	print("</div>");
+                print("</ul>\n");
+            } else {
+                $l = sizeof($errors);
+                $errors[$l]["url"] = $feed["url"];
+                $errors[$l]["x"] = $x;
+                $errors[$l]["y"] = $y;
+            }
+        }
+        print("</div>");
     }
 }
 
@@ -213,15 +213,15 @@ if (sizeof($errors) > 0) {
     print("</p>\n");
     print("<ul>\n");
     foreach ($errors as $error) {
-	print("<li>" . $error["url"]);
-	print("<span class=\"manage\">");
-	print("&nbsp;(<a class=\"manage\" href=\"");
-	$tmp = $column;
-	unset($tmp[$error["x"]][$error["y"]]);
-	print(htmlentities(getNewUrl($tmp)));
-	print("\">Del.</a>)");
-	print("</span>");
-	print("</li>\n");
+        print("<li>" . $error["url"]);
+        print("<span class=\"manage\">");
+        print("&nbsp;(<a class=\"manage\" href=\"");
+        $tmp = $column;
+        unset($tmp[$error["x"]][$error["y"]]);
+        print(htmlentities(getNewUrl($tmp)));
+        print("\">Del.</a>)");
+        print("</span>");
+        print("</li>\n");
     }
     print("</ul>\n");
     print("</div>\n");
