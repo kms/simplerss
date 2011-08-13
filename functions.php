2297a6655df02afb47d0fc3b8f64ec70bd338431
Karl-Martin Skontorp <kms@skontorp.net>
2011-08-13 14:06:00 +0200

    Update homepage reference.

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 # simplerss
 # Simple RSS viewer.
 #
-# (c) Karl-Martin Skontorp <kms@skontorp.net> ~ http://picofarad.org/
+# (c) Karl-Martin Skontorp <kms@skontorp.net> ~ http://22pf.org/
 # Licensed under the GNU GPL 2.0 or later.
 
 function printHeader() {
